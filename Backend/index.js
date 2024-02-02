4de3337dc9d15c9dbe769f8cc415f5a51b789cd5
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-02-02 23:09:56 +0600

    connection with database

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGODB)
+.then(()=>{
+console.log('mongoDB is connected')
+})
 const app = express()
 app.listen(3000, ()=>{
  console.log('3000 server is running');
