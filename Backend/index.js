fff89c795e853a3ad035c48ab5bab3158b25f767
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-02-03 23:59:29 +0600

    post work perfectly

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
+
 mongoose.connect(process.env.MONGODB)
 .then(()=>{
 console.log('mongoDB is connected')
 })
+
 const app = express()
+app.use(express.json());
+
 app.listen(3000, ()=>{
  console.log('3000 server is running');
 })
+app.use('/',userRoute)
+app.use('/',authRoute)
