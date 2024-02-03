c3ca94259a0146b21daeb6fc13ac91b2984173b2
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-02-04 01:01:00 +0600

    create signup api route

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,3 +1,33 @@
+import User from "../Model/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req,res)=>{
-    console.log(req.body);
-}
+      const { username, email, password } = req.body;
+      
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    return res.status(400).json({message:'all input are required'});
+  }
+  const hashPassword = bcryptjs.hashSync(password,10);
+
+  const newUser = new User({
+    username:username,
+    email: email,
+    password: hashPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } 
+  catch (error) {
+    return res.status(500).json({message:error.message});
+  }
+};
