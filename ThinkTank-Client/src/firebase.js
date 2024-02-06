575aef8fddfc692e52ee8123cdb53521107c347b
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-02-06 17:23:30 +0600

    add social Login

---
+++ ThinkTank-Client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+//   apiKey: import.mete.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDm6IGqNT30lo68kb0MDJRPT2W8zauLC_4",
+  authDomain: "thinktank-49cbb.firebaseapp.com",
+  projectId: "thinktank-49cbb",
+  storageBucket: "thinktank-49cbb.appspot.com",
+  messagingSenderId: "438043741190",
+  appId: "1:438043741190:web:b579d95d25c3bac38f8787"
+};
+console.log(firebaseConfig);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
