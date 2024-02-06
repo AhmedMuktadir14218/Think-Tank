b5c65cb6847d16deaee92403acbaf8ad917a7fef
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-02-06 14:23:00 +0600

    add redux persist

--- ThinkTank-Client/src/Pages/SignIn.jsx
+++ ThinkTank-Client/src/Pages/SignIn.jsx
@@ -1,40 +1,45 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../Redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (  !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
-      const res = await fetch('api/auth/signin', {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -49,15 +54,14 @@ export default function SignIn() {
             Tank
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-             
+             You can sign in with your email and password
+            or with Google.
           </p>
         </div>
         {/* right */}
 
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-            
             <div>
               <Label value='Your email' />
               <TextInput
@@ -71,7 +75,7 @@ export default function SignIn() {
               <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='Password'
+                placeholder='**********'
                 id='password'
                 onChange={handleChange}
               />
@@ -87,15 +91,15 @@ export default function SignIn() {
                   <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                'Sign Up'
+                'Sign In'
               )}
             </Button>
             {/* <OAuth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>create an account?</span>
+            <span>Dont Have an account?</span>
             <Link to='/sign-up' className='text-blue-500'>
-              Sign UP
+              Sign Up
             </Link>
           </div>
           {errorMessage && (
